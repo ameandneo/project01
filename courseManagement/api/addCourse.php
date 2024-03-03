09596b2177771c6cfa3ad8e6a1fc5693933b4583
ameandneo <a430193333@gmail.com>
2024-03-03 17:24:51 +0800

    寫進資料庫

---
+++ courseManagement/api/addCourse.php
@@ -0,0 +1,55 @@
+<?php
+include __DIR__ . '/../parts/PDOconnect.php';
+
+$dir = __DIR__ . '/../uploads/'; # 存放檔案的資料夾
+$exts = [   # 檔案類型的篩選 給附檔名
+    'image/jpeg' => '.jpg',
+    'image/png' =>  '.png',
+    'image/webp' => '.webp',
+];
+
+#先寫好要回應給用戶端的東西格式
+$output = [
+    'success' => false,
+    'file' => '', //成功後儲存的檔案名稱
+    'postData' => $_POST, #除錯用
+    'error' => '',
+    'code' => 0,
+];
+
+
+# 保存檔案，確保有上傳檔案，並且有imgFile 欄位，並且沒有錯誤
+if (!empty($_FILES) and !empty($_FILES['imgFile']) and $_FILES['imgFile']['error'] == 0) {
+    # 如果類型有對應到副檔名
+    $type = $_FILES['imgFile']['type']; //可能是image/jpeg,image/png或image/webp
+    if (!empty($exts[$type])) { # 如果上船的檔案類型有對應到$exts裡的值
+        $ext = $exts[$type]; # 副檔名
+        $f = sha1($_FILES['imgFile']['name'] . uniqid()); # 隨機的主檔名
+        if (move_uploaded_file($_FILES['imgFile']['tmp_name'], $dir . $f . $ext)) {
+            $output['success'] = true;
+            $output['file'] = $f . $ext;
+        }
+    }
+}
+//開始寫進資料庫
+// 避免SQL injection: 先prepare再execute，把單引號便跳脫字元
+$sql =
+    "INSERT INTO `course`(`title`, `intro`, `syllabus`, `teacherSN`, `courseImg`, `price`,`whenApply`,whenApproved) 
+VALUES (
+?,?,?,?,?,?,
+NOW(),NOW()
+)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    $_POST['title'],
+    $_POST['intro'],
+    $_POST['syllabus'],
+    $_POST['userID'],
+    $f . $ext,
+    $_POST['price'],
+]);
+$output['success'] = '成功的筆數' . $stmt->rowCount();
+
+
+header('Content-Type: application/json');
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
--- courseManagement/parts/html-create.php
+++ courseManagement/parts/html-create.php
@@ -13,6 +13,10 @@
             <label for="syllabus" class="form-label">課綱</label>
             <input type="text" class="form-control" id="syllabus" name="syllabus">
         </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">價格</label>
+            <input type="number" class="form-control" id="price" name="price">
+        </div>
 
         <select class="form-select" name="userID">
             <?php $allTeachers = $pdo->query('SELECT userName,userID FROM `user` WHERE isTeacher=1;')->fetchAll(PDO::FETCH_ASSOC); ?>
@@ -23,7 +27,7 @@
 
         <div class="mb-3">
             <label for="formFile" class="form-label">圖片</label>
-            <input class="form-control" type="file" id="formFile" accept="image/jpeg,image/png,image/webp" multiple="false" onchange="showTemp(event)" />
+            <input class="form-control" type="file" id="formFile" accept="image/jpeg,image/png,image/webp" multiple="false" onchange="showTemp(event)" name="imgFile" />
             <!-- 接受的檔案類型 -->
 
             <!-- 空的img-->
@@ -36,34 +40,35 @@
     const allTeachers = <?= json_encode($allTeachers, JSON_UNESCAPED_UNICODE) ?>;
     console.log(allTeachers);
 
-    // log看一下createCourse表單送出的資料
+    
 
     const myimg = document.querySelector("#myimg");
 
     function sendData(e) {
         e.preventDefault();
         // 防止送出表單重整
+        // log看一下createCourse表單送出的資料
         const createCourseFD = new FormData(document.createCourse);
         for (let i of createCourseFD.entries()) {
             console.log(i);
         };
         //發ajax
-
-        fetch('../api/addCourse.php', {
+        fetch('./api/addCourse.php', {
                 method: "POST",
                 body: createCourseFD,
             })
             .then((r) => r.json())
             .then((result) => {
                 if (result.success) {
-                    // alert('上傳成功');
+                    alert('已新增課程');
                     myimg.src = "./../uploads/" + result.file; //設定空的img的src屬性
-                    filename.value = result.file;
+                    
                 } else {
-                    alert("上傳失敗");
+                    alert("新增失敗");
                 }
             });
     }
+
     // 顯示用戶自己的圖
     let formFile = document.getElementById('formFile');
 
