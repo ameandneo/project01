fb7cb9cfbd0e0f18d2a16ed04a791fa4ca8243b8
ameandneo <a430193333@gmail.com>
2024-03-07 14:32:57 +0800

    search

--- courseManagement/parts/html-search.php
+++ courseManagement/parts/html-search.php
@@ -6,7 +6,25 @@ $keyword=
 "select * from course where availible = 1" ;
 "select * from course where approverID is null" ;
 
-"order by
-price
-sold amount
-"
+// "order by
+// price
+// sold amount
+// ";
+// 3.查詢結果排序 
+// 上架時間 新到舊
+// 價格 高到低
+// liked 高到低 
+// 課時
+
+
+
+// 搜尋
+// 課程名稱 course.title
+// 教師名 course.teacherSN==user.teacherSN  user.userName
+// 課程簡介  course.intro
+// 是否上架 course.available
+// 是否促銷中
+// 價格區間
+// 課時區間
+// 購買人數
+// 修畢人數
