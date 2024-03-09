515cfe8d0e1e37960b1e0471404bd35b0fc13eca
ameandneo <a430193333@gmail.com>
2024-03-09 22:19:15 +0800

    js改寫list頁

--- courseManagement/api/orderCourse.php
+++ courseManagement/api/orderCourse.php
@@ -1,3 +1,41 @@
 <?php
-$orderType = file_get_contents("php://input");
-echo json_encode($orderType);
+include __DIR__ . '/../parts/PDOconnect.php';
+
+$postData = file_get_contents("php://input");
+$data = json_decode($postData, true);
+
+$page=$data['page'];
+$orderValue=$data['orderValue'];
+$limitPerpage=$data['limitPerpage'];
+$otherLimit=$data['otherLimit'];
+#先寫好要回應給用戶端的東西格式
+// $output = [
+//     'success' => false,
+//     'file'=>'', //成功後儲存的檔案名稱
+//     'postData' => $_POST, #除錯用
+//     'error' => '',
+//     'code' => 0,
+// ];
+
+$output = [
+    'success' => false,
+    'postData' => $data, #除錯用
+];
+
+//得到總筆數
+$total_rows_sql = "SELECT count(1) FROM course c1 join user u1 on c1.teacherSN=u1.userID left join promotion p1 on c1.courseID= p1.courseID where promotionSN is null || CURRENT_DATE() BETWEEN whenStarted AND whenEnded";
+$total_rows_stmt = $pdo->query("$total_rows_sql");
+$totalRows = $total_rows_stmt->fetch(PDO::FETCH_NUM)[0]; 
+
+// 一頁有幾筆資料
+$totalPages = ceil($totalRows/$limitPerpage); #總頁數
+
+
+// 每頁的表格內容
+$rows = $pdo->query(sprintf("SELECT c1.courseID,title,intro,syllabus,teacherSN,courseImg,approverID,available,price,userName,promotionName,p1.percentage FROM course c1 join user u1 on c1.teacherSN=u1.userID left join promotion p1 on c1.courseID= p1.courseID where promotionSN is null || CURRENT_DATE() BETWEEN whenStarted AND whenEnded ORDER BY c1.$orderValue ASC LIMIT " . ($page - 1) * $limitPerpage . ",$limitPerpage"))->fetchAll(PDO::FETCH_ASSOC);
+
+$output['totalRows'] = $totalRows;
+$output['totalPages'] = $totalPages;
+$output['rows'] = $rows;
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
--- courseManagement/html-sort.php
+++ courseManagement/html-sort.php
@@ -5,45 +5,7 @@ include __DIR__ . '/parts/html-sidebar.php';
 ?>
 
 
-<?php
-
-// // 第一次$page為1
-$page  = isset($_GET['page']) ? intval($_GET['page']) : 1;
-if ($page < 1) {
-  header('Location: ?page=1');
-  exit;
-}
-
-
-$total_rows_sql = "SELECT count(1) from course";
-$total_rows_stmt = $pdo->query("$total_rows_sql");
-$totalRows = $total_rows_stmt->fetch(PDO::FETCH_NUM)[0]; //得到總筆數
-
-
-// 一頁有幾筆資料
-$perPage = 10;
-$totalPages = ceil($totalRows / $perPage); #總頁數
-
-
-if ($totalRows > 0) {
-  if ($page > $totalPages) {
-    header('Location: ?page=' . $totalPages); //若page大於200就跳到page=200，然後結束
-    exit;
-  }
-}
-
-// 排序
-$sql =
-
-  // 每頁的表格內容
-  $rows = $pdo->query(sprintf("SELECT c1.courseID,title,intro,syllabus,teacherSN,courseImg,approverID,available,price,userName,promotionName,p1.percentage FROM course c1 join user u1 on c1.teacherSN=u1.userID left join promotion p1 on c1.courseID= p1.courseID where promotionSN is null || CURRENT_DATE() BETWEEN whenStarted AND whenEnded ORDER BY c1.courseID ASC LIMIT " . ($page - 1) * $perPage . ",$perPage"))->fetchAll(PDO::FETCH_ASSOC);
-
-// 促銷狀態:SELECT * FROM course c1 join user u1 on c1.teacherSN=u1.userID left join promotion p1 on c1.courseID= p1.courseID where promotionSN is null || CURRENT_DATE() BETWEEN whenStarted AND whenEnded;
-
-// 若現在時間在促銷時間範圍內的promotion的courseID等於course.courseID
-
-// 選頁
-?><!-- 表格內容 -->
+=<!-- 表格內容 -->
 
 
 <div class="container-right">
@@ -52,7 +14,11 @@ $sql =
   <!-- 排序 -->
   <div class="hstack gap-3">
     <div class="p-2 rounded-4 bg-white py-2 function-wrap">
-      <h5>共<?= $totalRows ?>筆/每頁顯示<?= $perPage ?>筆</h5>
+      <h5>共<span id="limitPerpage"></span>筆/每頁顯示<select name="limitPerpageSelect" id="limitPerpageSelect">
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>筆</h5>
     </div>
     <div class="p-2 rounded-4 bg-white py-2 function-wrap">
       <h5>排序</h5>
@@ -104,75 +70,145 @@ $sql =
     <div class="table-data-wrap">
       <table class="table table-hover table-sm">
 
-        <tbody class="text-center">
-          <?php foreach ($rows as $r) : ?>
-            <tr>
-              <th style="width: 87px;" scope="row"><?= $r['courseID'] ?></th>
-              <td style="width: 550px;"><?= $r['title'] ?></td>
-              <td style="width: 120px;"><?= $r['userName'] ?></td>
-              <td style="width: 120px;"><?= $r['promotionName'] ? '<span class="text-decoration-line-through"style="font-size:16px" >' . $r['price'] . '</span><span class="text-warning">' . $r['price'] * $r['percentage'] / 100 . '</span>' : $r['price'] ?></td>
-              <td class="text-center"><?= $r['approverID'] ? ($r['available'] ? '<span class="text-success">已上架</span>' : '<span class="text-body-tertiary">下架</span>') : '<span class="text-danger">未審核</span>' ?></td>
-              <td class="text-center"><?= $r['promotionName'] ? '<span class="text-warning">' . $r["promotionName"] . '</span>' : "-" ?></td>
-              <td class="text-end"><?php
-                                    $soldRows = $pdo->query(sprintf("SELECT COUNT(*) FROM payment WHERE courseID =" . $r['courseID'] . ";"))->fetch(PDO::FETCH_NUM)[0]; #總筆數
-                                    echo $soldRows;
-                                    ?></td>
-              <td style="width: 123px;" class="text-center"><button class="btn <?= $r['approverID'] ? ($r['available'] ? 'btn-secondary' : 'btn-launch') : 'btn-success' ?>">
-                  <?= $r['approverID'] ? ($r['available'] ? '<i class="fa-solid fa-arrow-turn-down me-2"></i>下架' : '<i class="fa-solid fa-arrow-up-from-bracket me-2"></i>上架') : '<i class="fa-solid fa-check me-2"></i>核准' ?></button></td>
-              <!-- 編輯按鈕 -->
-              <td style="width: 123px;" class="text-center"><a class="btn btn-primary" href="edit.php?courseID=<?= $r['courseID'] ?> "><i class="fa-solid fa-pen-to-square me-2 "></i>編輯</a></td>
+        <tbody class="text-center" id="tbody">
 
-            </tr>
-            <tr>
-            <?php endforeach; ?>
         </tbody>
       </table>
     </div>
   </div>
   <!-- 選頁 -->
   <nav class="d-flex gap-3 page-nav align-items-center">
-    <a class="btn btn-primary rounded-5 <?= $page !== 1 ?: 'disabled';
-                                        //若已經是最前頁就關閉按鈕
-                                        ?>" href="?page=<?= $page - 1 ?>">
+    <a id="previousPage" class="btn btn-primary rounded-5">
       <button class="page-link">上一頁</button>
     </a>
-    <ul class="pagination -sm m-0">
-      <?php for ($i = $page - $perPage; $i <= $page + $perPage; $i++) : ?>
-        <!-- 選數字頁 -->
-        <?php if ($i >= 1 && $i <= $totalPages) : ?>
-
-          <li class="page-item <?= $i === $page ? 'active' : '' ?>">
-            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
-          </li>
-        <?php endif ?>
-      <?php endfor ?>
+    <ul class="pagination -sm m-0" id="pageNumber">
     </ul>
 
-    <a class="btn btn-primary rounded-5 
-        <?= $page == $totalPages ? 'disabled' : ''; ?>" href="?page=<?= $page + 1 ?>">
+    <a class="btn btn-primary rounded-5" id="nextPage">
       <button class="page-link ">下一頁</button>
     </a>
-    <span>共<?= $totalPages ?>頁 </span>
+    <span>共 頁 </span>
   </nav>
 </div>
 <!-- JS -->
 <script>
   let orderEl = document.getElementById("orderSelect");
-  let orderElValue;
-  orderEl.onchange = (e) => {
-    console.log(e.target.value);
-    orderElValue = e.target.value;
+  let limitPerpageEl = document.getElementById("limitPerpageSelect");
+  let orderValue = orderEl.value;
+  let limitPerpage = limitPerpageEl.value;
+  let currentPage = 1;
+  let previousPageBtn = document.getElementById('previousPage');
+  let nextPageBtn = document.getElementById('nextPage');
+  let otherLimit = 'otherLimit';
+  let tbody = document.getElementById('tbody');
+  let pageNumberUl = document.getElementById("pageNumber");
 
+  // then要用的變數
+  let rows;
+  let courseState;
+  let promotionState;
+  let btnApprove;
+
+  let loadData = (page, orderValue, limitPerpage, otherLimit) => {
+    let data = {
+      'page': page,
+      'orderValue': orderValue,
+      'limitPerpage': limitPerpage,
+      'otherLimit': otherLimit,
+    }
     fetch("api/orderCourse.php", {
       method: "POST",
       headers: {
-        "Content-Type": "text/plain",
+        'Content-Type': 'application/json'
       },
-      body: orderElValue
-    }).then(function(r){
-      console.log(r);
+      body: JSON.stringify(data)
+    }).then((r) => {
       return r.json();
-    }).then((r=>console.log(r))) //拿到$orderType
+    }).then((output) => {
+      console.log(output);
+      // 重置
+      tbody.innerHTML = '';
+      pageNumberUl.innerHTML = '';
+      previousPageBtn.classList.remove('disabled');
+      nextPageBtn.classList.remove('disabled');
+      rows = output['rows'];
+
+      for (let r of rows) {
+        if (r['available'] = 1) {
+          courseState = '<span class="text-success">已上架</span>';
+        } else if (r['approverID']) {
+          courseState = '<span class="text-body-tertiary">下架</span>';
+        } else {
+          courseState = '<span class="text-danger">未審核</span>'
+        }
+        promotionState = (r['promotionName'] ? `<span class="text-warning">${r["promotionName"]}</span>` : "-");
+        btnApprove = (r['approverID'] ? (r['available'] ? 'btn-secondary' : 'btn-launch') : 'btn-success');
+        btnApproveContent = (r['approverID'] ? (r['available'] ? '<i class="fa-solid fa-arrow-turn-down me-2"></i>下架' : '<i class="fa-solid fa-arrow-up-from-bracket me-2"></i>上架') : '<i class="fa-solid fa-check me-2"></i>核准');
+        tbody.innerHTML +=
+          `<tr>
+              <th style="width: 87px;" scope="row"> ${r['courseID']}</th>
+              <td style="width: 550px;">${r['title']} </td>
+              <td style="width: 120px;"> ${r['userName']} </td>
+              <td style="width: 120px;"> ${r['price']}</td>
+              <td class="text-center">${courseState}</td>
+              <td class="text-center">${r['promotionName']}</td>
+              <td class="text-end"></td>
+              <td style="width: 123px;" class="text-center"><button class="btn ${btnApprove}">${btnApproveContent}
+                   </button></td>
+              <!-- 編輯按鈕 -->
+              <td style="width: 123px;" class="text-center"><a class="btn btn-primary" href="edit.php?courseID= ${r['courseID']}"><i class="fa-solid fa-pen-to-square me-2 "></i>編輯</a></td>
+            </tr>`
+      }
+      if (currentPage === 1) {
+        previousPageBtn.classList.add('disabled')
+      }
+      if (currentPage === output['totalPages']) {
+        nextPageBtn.classList.add('disabled')
+      }
+      for (let i = currentPage - 3; i <= currentPage + 3; i++) {
+        if (i >= 1 && i <= output['totalPages']) {
+          if (i === currentPage) {
+            pageNumberUl.innerHTML +=
+              `<li class = "page-item active" >
+          <a class = "page-link" >${i}</a> </li>`
+          } else {
+            pageNumberUl.innerHTML +=
+              `<li class = "page-item" >
+          <a class = "page-link" >${i}</a> </li>`
+          }
+        }
+
+      }
+
+    });
+  }
+
+  previousPageBtn.onclick = (e) => {
+    if (currentPage > 1) {
+      currentPage--;
+      loadData(currentPage, orderValue, limitPerpage, otherLimit);
+    }
+  };
+  nextPageBtn.onclick = (e) => {
+    if (currentPage < 100 /*最大頁數 */ ) {
+      currentPage++;
+      loadData(currentPage, orderValue, limitPerpage, otherLimit);
+    }
+  };
+  orderEl.onchange = (e) => {
+    console.log(e.target.value);
+    orderValue = e.target.value;
+    loadData(currentPage, orderValue, limitPerpage, otherLimit)
+  };
+  limitPerpageEl.onchange = (e) => {
+    console.log(e.target.value);
+    limitPerpage = e.target.value;
+    loadData(currentPage, orderValue, limitPerpage, otherLimit)
+  };
+  pageNumberUl.onclick = (e) => {
+    console.log(Number(e.target.innerHTML));
+    currentPage=Number(e.target.innerHTML);
+    loadData(currentPage, orderValue, limitPerpage, otherLimit);
   }
 </script>
 <?php
