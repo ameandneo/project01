ea56a11d70856e0e84a857eeb0e9823b9ee66d35
ameandneo <a430193333@gmail.com>
2024-03-10 23:28:54 +0800

    db

--- courseManagement/database/whydb (1).sql
+++ courseManagement/database/whydb (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-03-03 16:02:38
+-- 產生時間： 2024-03-10 16:28:18
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -38,26 +38,46 @@ CREATE TABLE `course` (
   `available` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否上架',
   `price` int(11) NOT NULL,
   `whenApply` date NOT NULL COMMENT '送出申請時間',
-  `whenApproved` int(11) DEFAULT NULL COMMENT '審核通過時間'
+  `whenApproved` int(11) DEFAULT NULL COMMENT '審核通過時間',
+  `courseClassSN` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- 傾印資料表的資料 `course`
 --
 
-INSERT INTO `course` (`courseID`, `title`, `intro`, `syllabus`, `teacherSN`, `courseImg`, `approverID`, `available`, `price`, `whenApply`, `whenApproved`) VALUES
-(1, '課程1', 'intro	', 'syllabus', 1, NULL, NULL, 0, 2300, '2024-03-01', NULL),
-(2, '課程2', 'intro', 'syllabus', 1, NULL, 2, 1, 4555, '2024-03-01', NULL),
-(3, '核准未上架的課程', 'intro', 'intro', 1, NULL, 2, 0, 23333, '2024-03-01', NULL),
-(5, '12', '1', '2', 1, '', NULL, 0, 3, '2024-03-03', 2147483647),
-(6, '1', '2', '3', 1, '1fecd88b1be5fafb75c52417b12278786160ff30.jpg', NULL, 0, 33, '2024-03-03', 2147483647),
-(7, '1', '2', '3', 1, 'c5e5fba0bf278752716290d5c105ade64bcb11fb.png', NULL, 0, 33, '2024-03-03', 2147483647),
-(8, '1', '2', '3', 1, '70c4136607210936518fefacfa8211d562d9409a.jpg', NULL, 0, 33, '2024-03-03', 2147483647),
-(9, '1', '2', '4', 1, 'c031822d801a99a8ded4939daf3907fdb273926e.jpg', NULL, 0, 2222, '2024-03-03', 2147483647),
-(10, '1', '2', '5', 1, '9019f3465fb1e285d6b94f5090420b971ac5523d.jpg', NULL, 0, 7, '2024-03-03', 2147483647),
-(13, '1', '1', '1', 1, '', 2, 0, 1111, '2024-03-03', 2147483647),
-(14, '1', '1', '1', 1, '0d27ce39a400c5c8c8b4d959444363fa5e63a1b3.jpg', 2, 0, 1111, '2024-03-03', 2147483647),
-(15, '貓咪飼養1', '貓咪飼養', '貓咪飼養', 5, 'dc6927e8c6c3c94fdf7805512830ecfb90e088d2.jpg', 2, 0, 2000, '2024-03-03', 2147483647);
+INSERT INTO `course` (`courseID`, `title`, `intro`, `syllabus`, `teacherSN`, `courseImg`, `approverID`, `available`, `price`, `whenApply`, `whenApproved`, `courseClassSN`) VALUES
+(1, '未核准的課程', '1234', 'syllabus', 1, '', NULL, 0, 3000, '2024-03-01', NULL, 7),
+(2, '已上架課程2', '', 'syllabus', 1, '', 2, 1, 0, '2024-03-01', NULL, 6),
+(3, '核准未上架的課程', 'intro', 'intro', 1, NULL, 2, 0, 23333, '2024-03-01', NULL, 9),
+(15, '貓咪飼養1', '貓咪飼養', '貓咪飼養', 5, 'dc6927e8c6c3c94fdf7805512830ecfb90e088d2.jpg', 2, 0, 2000, '2024-03-03', 2147483647, 8),
+(21, '', '', '', 1, '', 2, 0, 0, '2024-03-10', 2147483647, 5);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `courseclass`
+--
+
+CREATE TABLE `courseclass` (
+  `courseClassSN` int(11) NOT NULL,
+  `className` text NOT NULL,
+  `whenAdded` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `whenEditted` timestamp NULL DEFAULT NULL,
+  `addedBy` int(11) NOT NULL,
+  `edittedBy` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `courseclass`
+--
+
+INSERT INTO `courseclass` (`courseClassSN`, `className`, `whenAdded`, `whenEditted`, `addedBy`, `edittedBy`) VALUES
+(5, '物理學', '2024-03-10 04:27:22', NULL, 2, NULL),
+(6, '歷史', '2024-03-10 04:30:24', NULL, 2, NULL),
+(7, '化學', '2024-03-10 04:27:33', NULL, 2, NULL),
+(8, '生物學', '2024-03-10 04:27:46', NULL, 2, NULL),
+(9, '地球科學', '2024-03-10 04:27:46', NULL, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -149,7 +169,16 @@ INSERT INTO `user` (`userID`, `isTeacher`, `userName`) VALUES
 ALTER TABLE `course`
   ADD PRIMARY KEY (`courseID`),
   ADD KEY `approverID` (`approverID`),
-  ADD KEY `teacherSN` (`teacherSN`);
+  ADD KEY `teacherSN` (`teacherSN`),
+  ADD KEY `courseClassSN` (`courseClassSN`);
+
+--
+-- 資料表索引 `courseclass`
+--
+ALTER TABLE `courseclass`
+  ADD PRIMARY KEY (`courseClassSN`),
+  ADD KEY `addedBy` (`addedBy`),
+  ADD KEY `deletedBy` (`edittedBy`);
 
 --
 -- 資料表索引 `employee`
@@ -188,7 +217,13 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `course`
 --
 ALTER TABLE `course`
-  MODIFY `courseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `courseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `courseclass`
+--
+ALTER TABLE `courseclass`
+  MODIFY `courseClassSN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `employee`
@@ -223,7 +258,15 @@ ALTER TABLE `user`
 --
 ALTER TABLE `course`
   ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`approverID`) REFERENCES `employee` (`employeeID`),
-  ADD CONSTRAINT `course_ibfk_2` FOREIGN KEY (`teacherSN`) REFERENCES `user` (`userID`);
+  ADD CONSTRAINT `course_ibfk_2` FOREIGN KEY (`teacherSN`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `course_ibfk_3` FOREIGN KEY (`courseClassSN`) REFERENCES `courseclass` (`courseClassSN`);
+
+--
+-- 資料表的限制式 `courseclass`
+--
+ALTER TABLE `courseclass`
+  ADD CONSTRAINT `courseclass_ibfk_1` FOREIGN KEY (`addedBy`) REFERENCES `employee` (`employeeID`),
+  ADD CONSTRAINT `courseclass_ibfk_2` FOREIGN KEY (`edittedBy`) REFERENCES `employee` (`employeeID`);
 
 --
 -- 資料表的限制式 `payment`
